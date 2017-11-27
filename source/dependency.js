cffa22c558f6042ee9054cbff3070b0ba6455671
James Rishe <jlrishe@users.noreply.github.com>
2017-11-27 14:59:01 +0900

    Add files

---
+++ source/dependency.js
@@ -0,0 +1,6 @@
+const constant = 'myProp';
+
+const a = { [constant]: 1 };
+const b = { [constant]: 2 };
+
+export { a, b };
---
+++ source/index.js
@@ -0,0 +1,4 @@
+// import babel helpers here so they are added to the global scope
+import './babel-helpers';
+
+export * from './dependency';
